01ecf05fca2fd5d9e8d27ed6f14bbb2bfdf89361
Warattapob <warattapobwa1@gmail.com>
2025-07-08 19:50:07 +0700

    python numbers

---
+++ python_numbers/python_numbers.py
@@ -0,0 +1,17 @@
+'''
+in python there are three numeric types.
+int
+float
+complex
+'''
+#int is a number positive or negative without decimals.
+x = 1
+#float is a number positive or negative with decimals.
+y = 2.8
+#complex is a imaginary part.
+z = 1j
+#convert from one type to another.
+a = float(x) #int to float.
+b = int(y) #float to int.
+c = complex(x) #int to complex
+
---
+++ python_numbers/python_random_number.py
@@ -0,0 +1,4 @@
+#How to random nuber in python
+import random
+print(random.randrange(1,10))
+#in code show 1-10 but in reality is 0,1,2,3....9
