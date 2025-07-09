2e45f4d668b6e142028ce79cf2684f49593d056b
Warattapob <warattapobwa1@gmail.com>
2025-07-09 21:14:03 +0700

    casting

---
+++ python_casting/python_casting.py
@@ -0,0 +1,13 @@
+#Casting is the process of converting the value of one type to valaue another type
+x = int(1)      # 1
+y = int(2.8)    # 2
+z = int("3")    # 3
+#flaot
+a = float(1)     # x will be 1.0
+b = float(2.8)   # y will be 2.8
+c = float("3")   # z will be 3.0
+d = float("4.2") # w will be 4.2
+#string
+q = str("s1") # x will be 's1'
+w = str(2)    # y will be '2'
+r = str(3.0)  # z will be '3.0'
