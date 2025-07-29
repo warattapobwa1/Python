02280ae3ce5cb7429004a78966d72a43b575d053
Warattapob <warattapobwa1@gmail.com>
2025-07-29 18:51:08 +0700

    29/7/2025

---
+++ python_operators/python_operators.py
@@ -0,0 +1,98 @@
+#python operators are used to perform operations on variables and values.
+'''
+group of python divides the operations
+1.Arithmetic operators
+2.Assignment operators
+3.Comparison operators
+4.Logical operators
+5.Identity operators
+6.Membership operators
+7.Bitwise operators
+'''
+#python arithmetic operators
+'''
++	Addition	x + y	
+-	Subtraction	x - y	
+*	Multiplication	x * y	
+/	Division	x / y	
+%	Modulus	x % y	
+**	Exponentiation	x ** y	
+//	Floor division	x // y
+'''
+#python assignment operators
+'''
+Operator	Example	Same As	
+=	x = 5	x = 5	
++=	x += 3	x = x + 3	
+-=	x -= 3	x = x - 3	
+*=	x *= 3	x = x * 3	
+/=	x /= 3	x = x / 3	
+%=	x %= 3	x = x % 3	
+//=	x //= 3	x = x // 3	
+**=	x **= 3	x = x ** 3	
+&=	x &= 3	x = x & 3	
+|=	x |= 3	x = x | 3	
+^=	x ^= 3	x = x ^ 3	
+>>=	x >>= 3	x = x >> 3	
+<<=	x <<= 3	x = x << 3	
+:=	print(x := 3)	x = 3
+print(x)
+'''
+#python comparison operators
+'''
+Operator	Name	Example	
+==	Equal	x == y	
+!=	Not equal	x != y	
+>	Greater than	x > y	
+<	Less than	x < y	
+>=	Greater than or equal to	x >= y	
+<=	Less than or equal to	x <= y
+'''
+#python logical operators
+'''
+Operator	Description	Example	
+and 	Returns True if both statements are true	x < 5 and  x < 10	
+or	Returns True if one of the statements is true	x < 5 or x < 4	
+not	Reverse the result, returns False if the result is true	not(x < 5 and x < 10)
+'''
+#python identity operators
+'''
+Operator	Description	Example	
+is 	Returns True if both variables are the same object	x is y	
+is not	Returns True if both variables are not the same object	x is not y
+'''
+#python membership operators
+'''
+Operator	Description	Example	
+in 	Returns True if a sequence with the specified value is present in the object	x in y	
+not in	Returns True if a sequence with the specified value is not present in the object	x not in y
+'''
+#python bitwise operators
+'''
+Operator	Name	Description	Example	
+& 	AND	Sets each bit to 1 if both bits are 1	x & y	
+|	OR	Sets each bit to 1 if one of two bits is 1	x | y	
+^	XOR	Sets each bit to 1 if only one of two bits is 1	x ^ y	
+~	NOT	Inverts all the bits	~x	
+<<	Zero fill left shift	Shift left by pushing zeros in from the right and let the leftmost bits fall off	x << 2	
+>>	Signed right shift	Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off	x >> 2
+'''
+#operator precedence
+'''
+parentheses has the highest precedence
+* higher than +
+Operator	Description	Try it
+()	Parentheses	
+**	Exponentiation	
++x  -x  ~x	Unary plus, unary minus, and bitwise NOT	
+*  /  //  %	Multiplication, division, floor division, and modulus	
++  -	Addition and subtraction	
+<<  >>	Bitwise left and right shifts	
+&	Bitwise AND	
+^	Bitwise XOR	
+|	Bitwise OR	
+==  !=  >  >=  <  <=  is  is not  in  not in 	Comparisons, identity, and membership operators	
+not	Logical NOT	
+and	AND	
+or	OR
+'''
